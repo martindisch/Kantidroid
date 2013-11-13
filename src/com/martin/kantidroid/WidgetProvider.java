d27910c01096c06efe457d61ca0a2db786baad1e
Martin Disch <martindisch@gmail.com>
2013-11-13 20:14:03 +0100

    New widget mostly done

--- src/com/martin/kantidroid/WidgetProvider.java
+++ src/com/martin/kantidroid/WidgetProvider.java
@@ -33,108 +33,29 @@ public class WidgetProvider extends AppWidgetProvider {
 		// Iterate through all instances of the widget
 		for (int y = 0; y < wcount; y++) {
 			// Get the intent for a click-event
-			Intent intent = new Intent(context, MainActivity.class);
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-					intent, 0);
+			Intent notIntent = new Intent(context, com.martin.noten.Main.class);
+			Intent kontIntent = new Intent(context, com.martin.kontingent.Overview.class);
+			Intent kissIntent = new Intent(context, com.martin.kiss.MainActivity.class);
+			Intent acnotIntent = new Intent(context, com.martin.noten.AddSelect.class);
+			Intent ackontIntent = new Intent(context, com.martin.kontingent.AddSelect.class);
+			
+			PendingIntent penNotIntent = PendingIntent.getActivity(context, 0, notIntent, 0);
+			PendingIntent penKontIntent = PendingIntent.getActivity(context, 0, kontIntent, 0);
+			PendingIntent penKissIntent = PendingIntent.getActivity(context, 0, kissIntent, 0);
+			PendingIntent penacNotIntent = PendingIntent.getActivity(context, 0, acnotIntent, 0);
+			PendingIntent penacKontIntent = PendingIntent.getActivity(context, 0, ackontIntent, 0);
 
 			// Get the views for the widget
 			RemoteViews views = new RemoteViews(context.getPackageName(),
 					R.layout.widget_layout);
 			// set the intent for the click-event
-			views.setOnClickPendingIntent(R.id.wLayout, pendingIntent);
+			views.setOnClickPendingIntent(R.id.llCardNoten, penNotIntent);
+			views.setOnClickPendingIntent(R.id.llCardKontingent, penKontIntent);
+			views.setOnClickPendingIntent(R.id.llCardKISS, penKissIntent);
+			views.setOnClickPendingIntent(R.id.ibNotenAdd, penacNotIntent);
+			views.setOnClickPendingIntent(R.id.ibKontAdd, penacKontIntent);
 
-			/*
 			// Noten
-
-			double plus = 0;
-			double minus = 0;
-			int total_minus = 0;
-			double total = 0;
-			int fcount = 0;
-			schn = 0;
-			boolean critical = false;
-			DatabaseHandler db = new DatabaseHandler(context);
-			int count = db.getFachCount();
-			List<Fach> faecher = db.getAllFaecher(context, 3);
-			for (int i = 0; i < count; i++) {
-				entry = faecher.get(i);
-				if (entry.getPromotionsrelevant().contentEquals("true")) {
-					// Falls beide Semester ausgefüllt
-					critical = true;
-					if (!entry.getRealAverage1().contentEquals("-")
-							&& !entry.getRealAverage2().contentEquals("-")) {
-						schn = (Double.parseDouble(entry.getRealAverage1()) + Double
-								.parseDouble(entry.getRealAverage2())) / 2;
-						if (schn > 4) {
-							plus += schn - 4;
-						}
-						if (schn < 4) {
-							minus += 4 - schn;
-							total_minus++;
-						}
-						total += (Double.parseDouble(entry.getMathAverage1()) + Double
-								.parseDouble(entry.getMathAverage2())) / 2;
-						fcount++;
-					}
-
-					// Falls nur erstes Semester ausgefüllt
-					if (entry.getRealAverage2().contentEquals("-")
-							&& !entry.getRealAverage1().contentEquals("-")) {
-						if (Double.parseDouble(entry.getRealAverage1()) > 4) {
-							plus += (Double
-									.parseDouble(entry.getRealAverage1()) - 4);
-						}
-						if (Double.parseDouble(entry.getRealAverage1()) < 4) {
-							minus += (4 - Double.parseDouble(entry
-									.getRealAverage1()));
-							total_minus++;
-						}
-						total += Double.parseDouble(entry.getMathAverage1());
-						fcount++;
-					}
-
-					// Falls nur zweites Semester ausgefüllt
-					critical = true;
-					if (!entry.getRealAverage2().contentEquals("-")
-							&& entry.getRealAverage1().contentEquals("-")) {
-						if (Double.parseDouble(entry.getRealAverage2()) > 4) {
-							plus += Double.parseDouble(entry.getRealAverage2()) - 4;
-						}
-						if (Double.parseDouble(entry.getRealAverage2()) < 4) {
-							minus += 4 - Double.parseDouble(entry
-									.getRealAverage2());
-							total_minus++;
-						}
-						total += Double.parseDouble(entry.getMathAverage2());
-						fcount++;
-					}
-				}
-			}
-			if (!((minus * 2) > plus)) {
-				if (total_minus <= 3) {
-					views.setTextColor(R.id.tvPP,
-							res.getColor(R.color.holo_green_light));
-				}
-				if (total_minus == 4) {
-					views.setTextColor(R.id.tvPP,
-							res.getColor(R.color.holo_orange_light));
-				}
-				if (total_minus > 4 && !critical) {
-					views.setTextColor(R.id.tvPP,
-							res.getColor(R.color.holo_orange_light));
-				}
-				if (total_minus > 4 && critical) {
-					views.setTextColor(R.id.tvPP,
-							res.getColor(R.color.holo_red_light));
-				}
-
-			} else {
-				views.setTextColor(R.id.tvPP,
-						res.getColor(R.color.holo_red_light));
-			}
-
-			double PP_result = plus - (2 * minus);
-			views.setTextViewText(R.id.tvPP, PP_result + "");
 
 			SharedPreferences spNoten = context.getSharedPreferences(
 					"MarkSettings", context.MODE_PRIVATE);
@@ -148,11 +69,14 @@ public class WidgetProvider extends AppWidgetProvider {
 			} else {
 				prResult = prCheck.getFMS(2);
 			}
-
-			String displayPP = prResult.sPP.split("/")[0];
-
-			views.setTextColor(R.id.tvPP, res.getColor(prResult.iColor));
-			views.setTextViewText(R.id.tvPP, displayPP);
+			views.setTextViewText(R.id.tvNotBig, "+ " + prResult.sPP.split("/")[0]);
+			/*
+			 * if (prResult.iColor == R.color.holo_green_light) {
+			 * pluspunkte.setTextColor(res.getColor(R.color.holo_orange_light));
+			 * } else { pluspunkte.setTextColor(res.getColor(prResult.iColor));
+			 * }
+			 */
+			views.setTextViewText(R.id.tvNotSmall, "Ø " + prResult.sSchnitt);
 
 			// Kontingent
 
@@ -162,7 +86,8 @@ public class WidgetProvider extends AppWidgetProvider {
 
 			int totalK = 0;
 			int used = 0;
-			int percentage = 0;
+			int iPercentage = 0;
+			double dPercentage = 0;
 
 			List<com.martin.kontingent.Fach> faecherK = dbK
 					.getAllFaecher(context);
@@ -187,18 +112,22 @@ public class WidgetProvider extends AppWidgetProvider {
 			}
 
 			if (!(totalK == 0)) {
-				percentage = (int) ((used * 100) / totalK);
+				iPercentage = (int) ((used * 100) / totalK);
+				dPercentage = (double) Math.round((double) used * 100 / totalK
+						* 100) / 100;
 			}
-
-			if (überzogen == 0) {
-				views.setTextColor(R.id.tvKont,
-						res.getColor(R.color.holo_green_light));
-			} else {
-				views.setTextColor(R.id.tvKont,
-						res.getColor(R.color.holo_red_light));
-			}
-
-			views.setTextViewText(R.id.tvKont, used + "/" + totalK);*/
+			views.setTextViewText(R.id.tvKontSmall, dPercentage + "%");
+			views.setTextViewText(R.id.tvKontBig, used + "/" + totalK);
+			/*
+			 * if (überzogen == 0) {
+			 * tvUsage.setTextColor(res.getColor(R.color.holo_orange_light)); }
+			 * else {
+			 * tvUsage.setTextColor(res.getColor(R.color.holo_red_light));
+			 * String before = tvUsage.getText().toString(); String einzmehrz =
+			 * " Fach überzogen"; if (überzogen > 1) { einzmehrz =
+			 * " Fächern überzogen"; } tvUsage.setText(before +
+			 * "\nKontingent in " + überzogen + einzmehrz); }
+			 */
 
 			// KISS
 
