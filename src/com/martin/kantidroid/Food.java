649106d06ffcfc15737c3754ca80d5c142063010
Martin Disch <martindisch@gmail.com>
2014-01-18 10:45:45 +0100

    Get and display json

--- src/com/martin/kantidroid/Food.java
+++ src/com/martin/kantidroid/Food.java
@@ -1,17 +1,18 @@
 package com.martin.kantidroid;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.holoeverywhere.app.Activity;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,6 +26,9 @@ public class Food extends Activity {
 	private EditText etRequest;
 	private Button bRequest;
 	private Typeface tf;
+	private String sOut = "";
+	private String[] weekdays = { "Montag", "Dienstag", "Mittwoch",
+			"Donnerstag", "Freitag", "Samstag", "Sonntag" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +46,8 @@ public class Food extends Activity {
 
 			@Override
 			public void onClick(View arg0) {
-				getResponse(getString(R.string.testpage), etRequest.getText().toString());
+				getResponse(getString(R.string.testpage), etRequest.getText()
+						.toString());
 			}
 
 		});
@@ -50,33 +55,65 @@ public class Food extends Activity {
 
 	private void getResponse(final String url, final String request) {
 
-		final String sReturn = "nil";
-
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				HttpClient httpclient = new DefaultHttpClient();
 
-				try {
-					final HttpResponse response = httpclient
-							.execute(new HttpGet(url + "?request=" + request));
+				// Get JSON response
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("request", request));
+				ServiceHandler sh = new ServiceHandler();
+				final String jsonStr = sh.makeServiceCall(url,
+						ServiceHandler.GET, params);
 
-					final String sTemp = EntityUtils.toString(
-							response.getEntity()).replace("<br>", "\n");
+				if (jsonStr != null && !jsonStr.contentEquals("invalid request")) {
+					try {
+						// Get top object, respresenting a single restaurant and containing all kalenderwochen
+						JSONObject rest = new JSONObject(jsonStr);
 
+						// Iterate through all kalenderwochen
+						Iterator<String> iter = rest.keys();
+
+						while (iter.hasNext()) {
+
+							String key = iter.next();
+							sOut += "Kalenderwoche " + key + "\n";
+							sOut += "==============\n\n";
+
+							// Get the current kalenderwochen object
+							final JSONObject kw = (JSONObject) rest.get(key);
+
+							// Iterate through all the days
+							for (String day : weekdays) {
+								sOut += day + "\n";
+								sOut += "************\n\n";
+								sOut += kw.getString(day) + "\n\n";
+							}
+							sOut += "\n";
+
+						}
+
+						runOnUiThread(new Runnable() {
+
+							@Override
+							public void run() {
+								tvOut.setText(sOut);
+								sOut = "";
+							}
+						});
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+				else {
 					runOnUiThread(new Runnable() {
-
+						
 						@Override
 						public void run() {
-							tvOut.setText(sTemp);
+							tvOut.setText("Invalid request");
 						}
 					});
-
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
 
 			}
