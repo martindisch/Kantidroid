e732a89c09d7fc98e38a7e9eb3eb6ae793a516f3
Martin Disch <martindisch@gmail.com>
2015-05-22 10:11:42 +0200

    Own layout

--- app/src/main/java/com/martin/kantidroid/NavigationDrawerFragment.java
+++ app/src/main/java/com/martin/kantidroid/NavigationDrawerFragment.java
@@ -101,7 +101,7 @@ public class NavigationDrawerFragment extends Fragment {
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
                 R.layout.simple_list_item_1,
-                android.R.id.text1,
+                R.id.text1,
                 new String[]{
                         getString(R.string.title_section1),
                         getString(R.string.title_section2),
