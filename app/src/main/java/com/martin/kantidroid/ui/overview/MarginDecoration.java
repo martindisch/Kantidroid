e170e21868766d0ab1431d232c2f436f625b4aaa
Martin Disch <martindisch@gmail.com>
2015-05-25 19:47:12 +0200

    Changed spacing in Overview, changed theme colors, build

--- app/src/main/java/com/martin/kantidroid/ui/overview/MarginDecoration.java
+++ app/src/main/java/com/martin/kantidroid/ui/overview/MarginDecoration.java
@@ -2,21 +2,85 @@ package com.martin.kantidroid.ui.overview;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import com.martin.kantidroid.R;
 
 public class MarginDecoration extends RecyclerView.ItemDecoration {
-  private int margin;
+    private int margin;
 
-  public MarginDecoration(Context context) {
-    margin = context.getResources().getDimensionPixelSize(R.dimen.item_margin);
-  }
+    public MarginDecoration(Context context) {
+        margin = context.getResources().getDimensionPixelSize(R.dimen.item_margin);
+    }
 
-  @Override
-  public void getItemOffsets(
-      Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-    outRect.set(margin, margin, margin, margin);
-  }
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int spacing = margin;
+        int halfSpacing = spacing / 2;
+
+        int childCount = parent.getChildCount();
+        int childIndex = parent.getChildAdapterPosition(view);
+        int spanCount = getTotalSpan(view, parent);
+        int spanIndex = childIndex % spanCount;
+
+        /* INVALID SPAN */
+        if (spanCount < 1) return;
+
+        outRect.top = spacing;
+        outRect.bottom = spacing;
+        outRect.left = halfSpacing;
+        outRect.right = halfSpacing;
+
+        if (isTopEdge(childIndex, spanCount)) {
+            outRect.top = 0;
+            outRect.bottom = 0;
+        }
+
+        if (isLeftEdge(spanIndex, spanCount)) {
+            outRect.left = 0;
+        }
+
+        if (isRightEdge(spanIndex, spanCount)) {
+            outRect.right = 0;
+        }
+
+        if (isBottomEdge(childIndex, childCount, spanCount)) {
+            outRect.bottom = 0;
+        }
+    }
+
+    protected int getTotalSpan(View view, RecyclerView parent) {
+
+        RecyclerView.LayoutManager mgr = parent.getLayoutManager();
+        if (mgr instanceof GridLayoutManager) {
+            return ((GridLayoutManager) mgr).getSpanCount();
+        } else if (mgr instanceof StaggeredGridLayoutManager) {
+            return ((StaggeredGridLayoutManager) mgr).getSpanCount();
+        }
+
+        return -1;
+    }
+
+    protected boolean isLeftEdge(int spanIndex, int spanCount) {
+
+        return spanIndex == 0;
+    }
+
+    protected boolean isRightEdge(int spanIndex, int spanCount) {
+
+        return spanIndex == spanCount - 1;
+    }
+
+    protected boolean isTopEdge(int childIndex, int spanCount) {
+
+        return childIndex < spanCount;
+    }
+
+    protected boolean isBottomEdge(int childIndex, int childCount, int spanCount) {
+
+        return childIndex >= childCount - spanCount;
+    }
 }
