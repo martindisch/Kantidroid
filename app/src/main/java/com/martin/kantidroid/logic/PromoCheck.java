babf2470feb50de0fb27c5c6a981295b3e16cd97
Martin Disch <martindisch@gmail.com>
2015-06-03 12:08:40 +0200

    Added PromoCheck

---
+++ app/src/main/java/com/martin/kantidroid/logic/PromoCheck.java
@@ -0,0 +1,320 @@
+package com.martin.kantidroid.logic;
+
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+
+import com.martin.kantidroid.R;
+
+public class PromoCheck {
+	private Context context;
+
+	public PromoCheck(Context context) {
+		super();
+		this.context = context;
+	}
+
+	public PromoRes getGym(int iSemester) {
+		Fach entry;
+		double plus = 0;
+		double minus = 0;
+		int total_minus = 0;
+		double schn = 0;
+		double total = 0;
+		int fcount = 0;
+		int mppcount = 0;
+
+		String sMessage = "Promoviert";
+		int iColor = R.color.primary_text_default_material_dark;
+		String sPP = "0.0";
+		String sSchnitt = "5.0000";
+
+		DatabaseHandler db = new DatabaseHandler(context);
+		int count = db.getFachCount();
+		List<Fach> faecher = db.getAllFaecher(context, iSemester);
+		for (int i = 0; i < count; i++) {
+			entry = faecher.get(i);
+			if (entry.getPromotionsrelevant().contentEquals("true")) {
+
+				switch (iSemester) {
+				case 1:
+					if (!entry.getRealAverage1().contentEquals("")) {
+						if (Double.parseDouble(entry.getRealAverage1()) > 4) {
+							plus += (Double.parseDouble(entry.getRealAverage1()) - 4);
+						}
+						if (Double.parseDouble(entry.getRealAverage1()) < 4) {
+							minus += (4 - Double.parseDouble(entry.getRealAverage1()));
+							total_minus++;
+						}
+						total += Double.parseDouble(entry.getMathAverage1());
+						fcount++;
+					}
+					break;
+				case 2:
+					if (!entry.getRealAverage2().contentEquals("")) {
+						if (Double.parseDouble(entry.getRealAverage2()) > 4) {
+							plus += Double.parseDouble(entry.getRealAverage2()) - 4;
+						}
+						if (Double.parseDouble(entry.getRealAverage2()) < 4) {
+							minus += 4 - Double.parseDouble(entry.getRealAverage2());
+							total_minus++;
+						}
+						total += Double.parseDouble(entry.getMathAverage2());
+						fcount++;
+					}
+					break;
+				case 3:
+					// Falls beide Semester ausgefüllt
+					if (!entry.getRealAverage1().contentEquals("") && !entry.getRealAverage2().contentEquals("")) {
+						schn = (Double.parseDouble(entry.getRealAverage1()) + Double.parseDouble(entry.getRealAverage2())) / 2;
+						if (schn > 4) {
+							plus += schn - 4;
+						}
+						if (schn < 4) {
+							minus += 4 - schn;
+							total_minus++;
+						}
+						total += (Double.parseDouble(entry.getMathAverage1()) + Double.parseDouble(entry.getMathAverage2())) / 2;
+						fcount++;
+					}
+
+					// Falls nur erstes Semester ausgefüllt
+					if (entry.getRealAverage2().contentEquals("") && !entry.getRealAverage1().contentEquals("")) {
+						if (Double.parseDouble(entry.getRealAverage1()) > 4) {
+							plus += (Double.parseDouble(entry.getRealAverage1()) - 4);
+						}
+						if (Double.parseDouble(entry.getRealAverage1()) < 4) {
+							minus += (4 - Double.parseDouble(entry.getRealAverage1()));
+							total_minus++;
+						}
+						total += Double.parseDouble(entry.getMathAverage1());
+						fcount++;
+					}
+
+					// Falls nur zweites Semester ausgefüllt
+					if (!entry.getRealAverage2().contentEquals("") && entry.getRealAverage1().contentEquals("")) {
+						if (Double.parseDouble(entry.getRealAverage2()) > 4) {
+							plus += Double.parseDouble(entry.getRealAverage2()) - 4;
+						}
+						if (Double.parseDouble(entry.getRealAverage2()) < 4) {
+							minus += 4 - Double.parseDouble(entry.getRealAverage2());
+							total_minus++;
+						}
+						total += Double.parseDouble(entry.getMathAverage2());
+						fcount++;
+					}
+					break;
+				}
+
+				mppcount++;
+			}
+		}
+		if (!((minus * 2) > plus)) {
+			if (total_minus <= 3) {
+				sMessage = "Promoviert";
+				iColor = R.color.promo_white;
+			}
+			if (total_minus == 4) {
+				sMessage = "Promoviert falls in OG\n4 ungenügende Noten";
+				iColor = R.color.promo_white;
+			}
+
+			switch (iSemester) {
+			case 1:
+				if (total_minus > 4) {
+					sMessage = "Mehr als 4 ungenügende Noten\nIm 2. Semester darf dies nicht der Fall sein";
+					iColor = R.color.promo_orange;
+				}
+				break;
+			case 2:
+				if (total_minus > 4) {
+					sMessage = "Nicht promoviert\nMehr als 4 ungenügende Noten";
+					iColor = R.color.promo_red;
+				}
+				break;
+			case 3:
+				if (getGym(1).sMessage.contains("Nicht promoviert") || getGym(2).sMessage.contains("Nicht promoviert")) {
+					sMessage = "Nicht promoviert";
+					iColor = R.color.promo_red;
+				}
+				break;
+			}
+		} else {
+			sMessage = "Nicht promoviert\nZu wenig Pluspunkte";
+			iColor = R.color.promo_red;
+		}
+
+		double PP_result = plus - (2 * minus);
+		sPP = PP_result + "/" + (mppcount * 2) + " Pluspunkte";
+
+		if (fcount > 0) {
+			sSchnitt = (String.format(Locale.getDefault(), "%.4f", total / fcount));
+		} else {
+			sSchnitt = ("-");
+		}
+
+		PromoRes prResult = new PromoRes(sMessage, iColor, sPP, sSchnitt);
+		return prResult;
+	}
+
+	public PromoRes getHMS(int iSemester) {
+		Fach entry;
+		double minus = 0;
+		int total_minus = 0;
+		double schn = 0;
+		double total = 0;
+		int fcount = 0;
+
+		String sMessage = "Promoviert";
+		int iColor = R.color.promo_white;
+		String sPP = "0.0";
+		String sSchnitt = "5.0000";
+
+		DatabaseHandler db = new DatabaseHandler(context);
+		int count = db.getFachCount();
+		List<Fach> faecher = db.getAllFaecher(context, iSemester);
+		for (int i = 0; i < count; i++) {
+			entry = faecher.get(i);
+			if (entry.getPromotionsrelevant().contentEquals("true")) {
+
+				// Falls beide Semester ausgefüllt
+				if (!entry.getRealAverage1().contentEquals("") && !entry.getRealAverage2().contentEquals("")) {
+					schn = (Double.parseDouble(entry.getRealAverage1()) + Double.parseDouble(entry.getRealAverage2())) / 2;
+					if (schn < 4) {
+						minus += 4 - schn;
+						total_minus++;
+					}
+					total += (Double.parseDouble(entry.getMathAverage1()) + Double.parseDouble(entry.getMathAverage2())) / 2;
+					fcount++;
+				}
+
+				// Falls nur erstes Semester ausgefüllt
+				if (entry.getRealAverage2().contentEquals("") && !entry.getRealAverage1().contentEquals("")) {
+					if (Double.parseDouble(entry.getRealAverage1()) < 4) {
+						minus += (4 - Double.parseDouble(entry.getRealAverage1()));
+						total_minus++;
+					}
+					total += Double.parseDouble(entry.getMathAverage1());
+					fcount++;
+				}
+
+				// Falls nur zweites Semester ausgefüllt
+				if (!entry.getRealAverage2().contentEquals("") && entry.getRealAverage1().contentEquals("")) {
+					if (Double.parseDouble(entry.getRealAverage2()) < 4) {
+						minus += 4 - Double.parseDouble(entry.getRealAverage2());
+						total_minus++;
+					}
+					total += Double.parseDouble(entry.getMathAverage2());
+					fcount++;
+				}
+			}
+		}
+		if (!((total / fcount) < 4)) {
+			if (total_minus <= 3) {
+				sMessage = "Promoviert";
+				iColor = R.color.promo_white;
+			} else {
+				sMessage = "Nicht promoviert\nMehr als 3 Ungenügende";
+				iColor = R.color.promo_red;
+			}
+			if (minus > 2.5) {
+				sMessage = "Nicht Promoviert\nMehr als 2.5 Minuspunkte";
+				iColor = R.color.promo_red;
+			}
+		} else {
+			sMessage = "Nicht promoviert\nGesamtschnitt unter 4";
+			iColor = R.color.promo_red;
+		}
+
+		// HMS, also keine Pluspunkte anzeigen
+		sPP = "-";
+
+		if (fcount > 0) {
+			sSchnitt = (String.format(Locale.getDefault(), "%.4f", total / fcount));
+		} else {
+			sSchnitt = ("-");
+		}
+
+		PromoRes prResult = new PromoRes(sMessage, iColor, sPP, sSchnitt);
+		return prResult;
+	}
+
+	public PromoRes getFMS(int iSemester) {
+		Fach entry;
+		double minus = 0;
+		double schn = 0;
+		double total = 0;
+		double total_real = 0;
+		int fcount = 0;
+
+		String sMessage = "Promoviert";
+		int iColor = R.color.promo_white;
+		String sPP = "0.0";
+		String sSchnitt = "5.0000";
+
+		DatabaseHandler db = new DatabaseHandler(context);
+		int count = db.getFachCount();
+		List<Fach> faecher = db.getAllFaecher(context, iSemester);
+		for (int i = 0; i < count; i++) {
+			entry = faecher.get(i);
+			if (entry.getPromotionsrelevant().contentEquals("true")) {
+
+				// Falls beide Semester ausgefüllt
+				if (!entry.getRealAverage1().contentEquals("") && !entry.getRealAverage2().contentEquals("")) {
+					schn = (Double.parseDouble(entry.getRealAverage1()) + Double.parseDouble(entry.getRealAverage2())) / 2;
+					if (schn < 4) {
+						minus += 4 - schn;
+					}
+					total += (Double.parseDouble(entry.getMathAverage1()) + Double.parseDouble(entry.getMathAverage2())) / 2;
+					total_real += (Double.parseDouble(entry.getRealAverage1()) + Double.parseDouble(entry.getRealAverage2())) / 2;
+					fcount++;
+				}
+
+				// Falls nur erstes Semester ausgefüllt
+				if (entry.getRealAverage2().contentEquals("") && !entry.getRealAverage1().contentEquals("")) {
+					if (Double.parseDouble(entry.getRealAverage1()) < 4) {
+						minus += (4 - Double.parseDouble(entry.getRealAverage1()));
+					}
+					total += Double.parseDouble(entry.getMathAverage1());
+					total_real += Double.parseDouble(entry.getRealAverage1());
+					fcount++;
+				}
+
+				// Falls nur zweites Semester ausgefüllt
+				if (!entry.getRealAverage2().contentEquals("") && entry.getRealAverage1().contentEquals("")) {
+					if (Double.parseDouble(entry.getRealAverage2()) < 4) {
+						minus += 4 - Double.parseDouble(entry.getRealAverage2());
+					}
+					total += Double.parseDouble(entry.getMathAverage2());
+					total_real += Double.parseDouble(entry.getRealAverage2());
+					fcount++;
+				}
+			}
+		}
+		if (!(minus > 2.5)) {
+			if (!((total_real / fcount) < 4)) {
+				sMessage = "Promoviert";
+				iColor = R.color.promo_white;
+			} else {
+				sMessage = "Nicht Promoviert\nPromotionsschnitt unter 4";
+				iColor = R.color.promo_red;
+			}
+		} else {
+			sMessage = "Nicht promoviert\nMehr als 2.5 Minuspunkte";
+			iColor = R.color.promo_red;
+		}
+
+		// FMS, also keine Pluspunkte anzeigen
+		sPP = "-";
+
+		if (fcount > 0) {
+			sSchnitt = (String.format(Locale.getDefault(), "%.4f", total / fcount));
+		} else {
+			sSchnitt = ("-");
+		}
+
+		PromoRes prResult = new PromoRes(sMessage, iColor, sPP, sSchnitt);
+		return prResult;
+	}
+}
---
+++ app/src/main/java/com/martin/kantidroid/logic/PromoRes.java
@@ -0,0 +1,34 @@
+package com.martin.kantidroid.logic;
+
+/**
+ * Represents the user's current situation
+ * 
+ * @author Martin
+ * 
+ */
+public class PromoRes {
+	public String sMessage;
+	public int iColor;
+	public String sPP;
+	public String sSchnitt;
+
+	/**
+	 * The constructor
+	 * 
+	 * @param sMessage
+	 *            Whether user is 'promoviert' or not
+	 * @param iColor
+	 *            The color matching the result
+	 * @param sPP
+	 *            The number of Pluspunkte
+	 * @param sSchnitt
+	 *            The user's average marks
+	 */
+	public PromoRes(String sMessage, int iColor, String sPP, String sSchnitt) {
+		super();
+		this.sMessage = sMessage;
+		this.iColor = iColor;
+		this.sPP = sPP;
+		this.sSchnitt = sSchnitt;
+	}
+}
