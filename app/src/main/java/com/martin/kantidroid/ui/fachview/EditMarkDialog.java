9018ad115d166dbab8ca4e40632acb0c2d4f4a8f
Martin Disch <martindisch@gmail.com>
2015-06-19 10:33:39 +0200

    Created EditMarkDialog layout as copy of EditDialog from subjects

---
+++ app/src/main/java/com/martin/kantidroid/ui/fachview/EditMarkDialog.java
@@ -0,0 +1,17 @@
+package com.martin.kantidroid.ui.fachview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.martin.kantidroid.R;
+
+public class EditMarkDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fachview_edit_mark_dialog);
+    }
+}
