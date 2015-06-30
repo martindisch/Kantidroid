94103f9e274be4d96c1fcc8f7329febd9f1f57e4
Martin Disch <martindisch@gmail.com>
2015-06-30 09:50:39 +0200

    Made title beautiful

--- app/src/main/java/com/martin/kantidroid/ui/util/ChangelogFragment.java
+++ app/src/main/java/com/martin/kantidroid/ui/util/ChangelogFragment.java
@@ -1,18 +1,26 @@
 package com.martin.kantidroid.ui.util;
 
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class ChangelogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Changelog");
+        TextView tv = new TextView(getActivity());
+        tv.setText("Changelog");
+        tv.setTextSize(20);
+        tv.setTextColor(Color.BLACK);
+        tv.setPadding(30, 30, 30, 30);
+        builder.setCustomTitle(tv);
         builder.setNeutralButton("Schliessen", null);
 
         WebView wv = new WebView(getActivity());
