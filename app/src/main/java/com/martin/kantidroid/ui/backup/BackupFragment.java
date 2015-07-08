cc953b978b5f72010881553d2369732e0684e38f
Martin Disch <martindisch@gmail.com>
2015-07-08 10:08:28 +0200

    Layout for local backup

--- app/src/main/java/com/martin/kantidroid/ui/backup/BackupFragment.java
+++ app/src/main/java/com/martin/kantidroid/ui/backup/BackupFragment.java
@@ -1,14 +1,9 @@
 package com.martin.kantidroid.ui.backup;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -16,16 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.martin.kantidroid.R;
-import com.martin.kantidroid.logic.DatabaseHandler;
-import com.martin.kantidroid.logic.Fach;
-import com.martin.kantidroid.ui.subjects.EditDialog;
-import com.martin.kantidroid.ui.subjects.SubjectsAdapter;
 import com.martin.kantidroid.ui.util.DividerItemDecoration;
-
-import java.util.List;
+import com.martin.kantidroid.ui.util.LinearLayoutManager;
 
 public class BackupFragment extends Fragment {
 
+    private RecyclerView mSelection;
 
     public static BackupFragment newInstance() {
         return new BackupFragment();
@@ -52,6 +43,19 @@ public class BackupFragment extends Fragment {
         ab.setHomeAsUpIndicator(R.drawable.ic_menu);
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setTitle(R.string.backup);
+
+        mSelection = (RecyclerView) rootView.findViewById(R.id.rvLocal);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mSelection.setLayoutManager(layoutManager);
+        mSelection.setHasFixedSize(true);
+        mSelection.addItemDecoration(new DividerItemDecoration(getActivity(), null, false));
+        BackupAdapter adapter = new BackupAdapter(getActivity(), new BackupAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+
+            }
+        });
+        mSelection.setAdapter(adapter);
 
         return rootView;
     }
