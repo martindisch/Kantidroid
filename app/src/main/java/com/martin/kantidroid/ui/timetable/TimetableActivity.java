b16198a8554fc6607e25f55c7003536552dea6bb
Martin Disch <martindisch@gmail.com>
2015-08-04 17:46:44 +0200

    Showing placeholder if no items

--- app/src/main/java/com/martin/kantidroid/ui/timetable/TimetableActivity.java
+++ app/src/main/java/com/martin/kantidroid/ui/timetable/TimetableActivity.java
@@ -1,18 +1,59 @@
 package com.martin.kantidroid.ui.timetable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.martin.kantidroid.R;
 
 public class TimetableActivity extends AppCompatActivity {
+
+    private TextInputLayout mTilClass;
+    private EditText mClass;
+    private Button mDownload;
+    private View mLayoutImage, mLayoutList;
+    private RecyclerView mDownloads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mTilClass = (TextInputLayout) findViewById(R.id.tilClass);
+        mClass = (EditText) findViewById(R.id.etClass);
+        mDownload = (Button) findViewById(R.id.bDownload);
+        mLayoutImage = findViewById(R.id.llImage);
+        mLayoutList = findViewById(R.id.flList);
+        mDownloads = (RecyclerView) findViewById(R.id.rvDownloads);
+
+        if (/*!hasDownloads*/ true) {
+            mLayoutList.setVisibility(View.INVISIBLE);
+        } else {
+            mLayoutImage.setVisibility(View.INVISIBLE);
+        }
+        Glide.with(this).load(R.drawable.kanti).into((ImageView) findViewById(R.id.ivNothing));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Let's not reload the main activity
+        onBackPressed();
+        return true;
     }
 
 }
