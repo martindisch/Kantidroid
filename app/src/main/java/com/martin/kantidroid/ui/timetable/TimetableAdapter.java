ef969c9a6b3e2d05926f1b3ae0c418d47b679dbb
Martin Disch <martindisch@gmail.com>
2015-08-06 15:41:51 +0200

    Using own color

--- app/src/main/java/com/martin/kantidroid/ui/timetable/TimetableAdapter.java
+++ app/src/main/java/com/martin/kantidroid/ui/timetable/TimetableAdapter.java
@@ -44,7 +44,7 @@ public class TimetableAdapter extends RecyclerView.Adapter<TimetableAdapter.View
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.tvName.setText(mEntries.get(position).getName());
-        holder.tvPic.getBackground().setColorFilter(mContext.getResources().getColor(R.color.material_blue_grey_800), PorterDuff.Mode.SRC_ATOP);
+        holder.tvPic.getBackground().setColorFilter(mContext.getResources().getColor(R.color.blue_grey), PorterDuff.Mode.SRC_ATOP);
         holder.tvPic.setText(mEntries.get(position).getName().substring(0, 1));
         holder.rlRoot.setOnClickListener(new View.OnClickListener() {
             @Override
