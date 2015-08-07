c8423283d92992506ecf319d6410cf15c89e5ef3
Martin Disch <martindisch@gmail.com>
2015-08-07 15:06:31 +0200

    Feedback activity

---
+++ app/src/main/java/com/martin/kantidroid/ui/feedback/FeedbackActivity.java
@@ -0,0 +1,34 @@
+package com.martin.kantidroid.ui.feedback;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.martin.kantidroid.R;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feedback_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return true;
+    }
+}
