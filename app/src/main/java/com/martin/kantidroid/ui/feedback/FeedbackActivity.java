246249f26e57d69346c6daa39296948160ff8025
Martin Disch <martindisch@gmail.com>
2015-08-07 20:40:28 +0200

    Layout for List

--- app/src/main/java/com/martin/kantidroid/ui/feedback/FeedbackActivity.java
+++ app/src/main/java/com/martin/kantidroid/ui/feedback/FeedbackActivity.java
@@ -4,15 +4,21 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.martin.kantidroid.R;
 import com.martin.kantidroid.logic.Util;
+import com.martin.kantidroid.ui.backup.BackupAdapter;
+import com.martin.kantidroid.ui.util.DividerItemDecoration;
+import com.martin.kantidroid.ui.util.LinearLayoutManager;
 
 public class FeedbackActivity extends AppCompatActivity {
 
@@ -34,6 +40,23 @@ public class FeedbackActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(getResources().getColor(R.color.feedback_statusbar));
         }
+
+        RecyclerView mSelection = (RecyclerView) findViewById(R.id.rvFeedback);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mSelection.setLayoutManager(layoutManager);
+        mSelection.setHasFixedSize(true);
+        mSelection.addItemDecoration(new DividerItemDecoration(this, null, false));
+        FeedbackAdapter adapter = new FeedbackAdapter(this, new FeedbackAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                if (position == 0) {
+
+                } else {
+
+                }
+            }
+        });
+        mSelection.setAdapter(adapter);
     }
 
     @Override
---
+++ app/src/main/java/com/martin/kantidroid/ui/feedback/FeedbackAdapter.java
@@ -0,0 +1,68 @@
+package com.martin.kantidroid.ui.feedback;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.martin.kantidroid.R;
+
+public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.ViewHolder> {
+
+    private String[] mTitles, mMessages;
+    private int[] mDrawables;
+    private final OnClickListener mCallback;
+
+    public FeedbackAdapter(Context context, OnClickListener callback) {
+        mTitles = new String[]{context.getString(R.string.backup_title_1), context.getString(R.string.backup_title_2)};
+        mMessages = new String[]{context.getString(R.string.backup_msg_1), context.getString(R.string.backup_msg_2)};
+        mDrawables = new int[]{R.drawable.ic_backup, R.drawable.ic_import};
+        mCallback = callback;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feedback_item, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.tvTitle.setText(mTitles[position]);
+        holder.tvMessage.setText(mMessages[position]);
+        holder.ivIcon.setImageResource(mDrawables[position]);
+        holder.rlRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.onItemClick(view, position);
+            }
+        });
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView tvTitle;
+        public final TextView tvMessage;
+        public final ImageView ivIcon;
+        public final View rlRoot;
+
+        public ViewHolder(View v) {
+            super(v);
+            tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+            tvMessage = (TextView) v.findViewById(R.id.tvMessage);
+            ivIcon = (ImageView) v.findViewById(R.id.ivIcon);
+            rlRoot = v.findViewById(R.id.rlRoot);
+        }
+    }
+
+    public interface OnClickListener {
+        void onItemClick(View v, final int position);
+    }
+}
