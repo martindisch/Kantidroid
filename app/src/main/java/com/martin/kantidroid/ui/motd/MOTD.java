71f09e54a46f73b44cfdf6bc13adf80b9a9c4b76
Martin Disch <martindisch@gmail.com>
2015-08-19 21:43:26 +0200

    Fixed layout

--- app/src/main/java/com/martin/kantidroid/ui/motd/MOTD.java
+++ app/src/main/java/com/martin/kantidroid/ui/motd/MOTD.java
@@ -1,7 +1,9 @@
 package com.martin.kantidroid.ui.motd;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.martin.kantidroid.R;
@@ -12,6 +14,9 @@ public class MOTD extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motd_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         String[] list = getIntent().getStringArrayExtra("data");
 
