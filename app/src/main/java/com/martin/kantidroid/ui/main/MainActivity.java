8e00fde8da59e3b0377504af40ad15cacbb852e8
Martin Disch <martindisch@gmail.com>
2015-08-20 09:45:31 +0200

    Removed unused placeholder fragment

--- app/src/main/java/com/martin/kantidroid/ui/main/MainActivity.java
+++ app/src/main/java/com/martin/kantidroid/ui/main/MainActivity.java
@@ -88,8 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateContainer() {
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment;
-        String tag;
+        Fragment fragment = null;
+        String tag = null;
         if (mSelected != mCurrent) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
             switch (mSelected) {
@@ -113,10 +113,6 @@ public class MainActivity extends AppCompatActivity {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
                     fragment = BackupFragment.newInstance();
                     tag = "backup";
-                    break;
-                default:
-                    fragment = PlaceholderFragment.newInstance(mSelected);
-                    tag = "placeholder";
                     break;
             }
             mCurrent = mSelected;
@@ -190,39 +186,6 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("mSelected", mSelected);
         outState.putInt("mCurrent", mCurrent);
         super.onSaveInstanceState(outState);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.dummy_fragment, container, false);
-        }
-
     }
 
 }
