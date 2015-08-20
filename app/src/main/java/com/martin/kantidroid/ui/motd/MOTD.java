f1de3b9daac5ead5614a25b065b1f60fcc1b0583
Martin Disch <martindisch@gmail.com>
2015-08-20 14:06:33 +0200

    Took the easy way out

--- app/src/main/java/com/martin/kantidroid/ui/motd/MOTD.java
+++ app/src/main/java/com/martin/kantidroid/ui/motd/MOTD.java
@@ -1,8 +1,10 @@
 package com.martin.kantidroid.ui.motd;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.martin.kantidroid.R;
@@ -17,6 +19,9 @@ public class MOTD extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         String[] list = getIntent().getStringArrayExtra("data");
 
         TextView tvTitle = (TextView) findViewById(R.id.tvMOTD_title);
@@ -29,5 +34,15 @@ public class MOTD extends AppCompatActivity {
 
     String unescape(String s) {
         return s.replace("*", "\n");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return true;
     }
 }
