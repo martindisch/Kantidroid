d84bfc99e6965973da21acefdd5c0ef8ee910287
Martin Disch <martindisch@gmail.com>
2015-08-27 12:32:37 +0200

    Tapping grades or kont opens App

--- app/src/main/java/com/martin/kantidroid/ui/widget/WidgetProvider.java
+++ app/src/main/java/com/martin/kantidroid/ui/widget/WidgetProvider.java
@@ -19,14 +19,15 @@ public class WidgetProvider extends AppWidgetProvider {
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            // Create an Intent to launch ExampleActivity
+            // Create an Intent to launch MainActivity
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
+            // Get the layout for the App Widget and attach on-click listeners
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
             views.setOnClickPendingIntent(R.id.llTop, pendingIntent);
+            views.setOnClickPendingIntent(R.id.widget_pp, pendingIntent);
+            views.setOnClickPendingIntent(R.id.widget_kont, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
